7b002e919632b5340378413c7586fd4f7436a874
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 03:26:00 -0700

    int to char test

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 03:17:19 by seli              #+#    #+#             */
+/*   Updated: 2018/09/20 03:25:40 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_comb2(void)
+{
+	int first;
+	int second;
+
+	first = 0;
+	while (first <= 98)
+	{
+		second = first + 1;
+		while (second <= 99)
+		{
+			ft_putchar(first / 10 + '0');
+			ft_putchar(first % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(second / 10 + '0');
+			ft_putchar(second % 10 + '0');
+			if (!(first == 98 && second == 99))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			second++;
+		}
+	}
+}
