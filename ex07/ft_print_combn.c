793668d00351b1042e4b0eea92977bd4a9c06cd6
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 05:01:12 -0700

    global value try

---
+++ ex07/ft_print_combn.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 03:54:54 by seli              #+#    #+#             */
+/*   Updated: 2018/09/20 05:00:56 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_print_combn_recursive(int n, int min);
+void	ft_print_prev(void);
+
+int g_max;
+int g_prev[10];
+
+void	ft_print_combn(int n)
+{
+	g_max = n;
+	ft_print_combn_recursive(n, 0);
+}
+
+void	ft_print_prev(void)
+{
+	int i;
+
+	i = 0;
+	while (i < g_max - 1)
+	{
+		ft_putchar(g_prev[i++] + '0');
+	}
+}
+
+void	ft_print_combn_recursive(int n, int min)
+{
+	int cur_min;
+
+	if (n == 1)
+	{
+		ft_print_prev();
+		ft_putchar(min + '0');
+		if (prev[0] == 10 - g_max)
+			return ;
+		ft_putchar(',');
+		ft_putchar(' ');
+		return ;
+	}
+	cur_min = min;
+	while (cur_min + n <= 10)
+	{
+		g_prev[g_max - n] = min;
+		ft_print_combn_recursive(n - 1, cur_min + 1);
+		cur_min++;
+	}
+	ft_print_combn_recursive(n, min + 1);
+}
