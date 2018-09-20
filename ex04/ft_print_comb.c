e0e8bc2cd7ba5c52b2f9d72d07a1aa1e564f184c
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 02:34:15 -0700

    mv to ex04

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 02:12:34 by seli              #+#    #+#             */
+/*   Updated: 2018/09/20 02:29:33 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_comb(void)
+{
+	char first;
+	char second;
+	char third;
+
+	first = '0';
+	second = '1';
+	third = '2';
+	while (first <= '7')
+	{
+		while (second <= '8')
+		{
+			while (third <= '9')
+			{
+				ft_putchar(first);
+				ft_putchar(second);
+				ft_putchar(third);
+				third++;
+			}
+			second++;
+			third = second + 1;
+		}
+		first++;
+		second = first + 1;
+		thrid = second + 1;
+	}
+}
