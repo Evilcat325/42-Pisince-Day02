5efe4933d1f4372e58c9697bf257d899581b88fa
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 03:51:48 -0700

    update method

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 03:27:50 by seli              #+#    #+#             */
-/*   Updated: 2018/09/20 03:42:11 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/20 03:51:09 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@ void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	int divisor;
-	int c;
+	long	divisor;
+	int		c;
 
 	if (nb <= 0)
 		ft_putchar('-');
-	divisor = 1000000000;
+	divisor = 10000000000;
+	while (nb > divisor)
+	{
+		divisor /= 10;
+	}
 	while (divisor != 1)
 	{
 		c = (nb / divisor) % 10;
+		if (c < 0)
+			c = -c;
 		if (c)
 			ft_putchar(c + '0');
 		divisor /= 10;
