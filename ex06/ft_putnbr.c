164929c55c4303ddb28676b177387149c60e8ea6
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 22:18:19 -0700

    update divisor logic

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 03:27:50 by seli              #+#    #+#             */
-/*   Updated: 2018/09/20 03:53:51 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/20 22:18:03 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@ void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	long	divisor;
-	int		c;
+	int	divisor;
+	int	c;
 
 	if (nb < 0)
 		ft_putchar('-');
-	divisor = 10000000000;
-	while (nb > divisor)
+	divisor = 1000000000;
+	while (nb < divisor)
 	{
 		divisor /= 10;
 	}
@@ -29,8 +29,7 @@ void	ft_putnbr(int nb)
 		c = (nb / divisor) % 10;
 		if (c < 0)
 			c = -c;
-		if (c)
-			ft_putchar(c + '0');
+		ft_putchar(c + '0');
 		divisor /= 10;
 	}
 	c = nb % 10;
